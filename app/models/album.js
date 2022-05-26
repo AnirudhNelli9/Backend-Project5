cca7aba9a83d0130224f7be4edebf041a92fc119
AnirudhNelli9 <anirudhvinay.nelli@eagles.oc.edu>
2022-05-26 13:54:13 -0500

    #commit - controller album .
    Co-Authored-By: SuryaNuthalapati <91345543+SuryaNuthalapati@users.noreply.github.com>
    Co-Authored-By: jagadeep9030 <105335600+jagadeep9030@users.noreply.github.com>

---
+++ app/models/album.js
@@ -0,0 +1,11 @@
+module.exports = (sequelize, Sequelize) => {
+    const Album = sequelize.define("album", {
+      title: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.STRING
+      }
+    });
+    return Album;
+  };
