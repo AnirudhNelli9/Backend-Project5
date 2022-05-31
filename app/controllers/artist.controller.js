582e456afae049746825381df6d8e17f96b2779e
SuryaNuthalapati <suryanuthalapati11@gmail.com>
2022-05-31 11:04:39 -0600

    Controller and routes

--- app/controllers/artist.controller.js
+++ app/controllers/artist.controller.js
@@ -1,3 +1,44 @@
 const db = require("../models");
 const Lesson = db.lessons;
 const Op = db.Sequelize.Op;
+
+// Delete a Artist with the specified id in the request
+exports.delete = (req, res) => {
+    const id = req.params.id;
+    Artist.destroy({
+      where: { id: id }
+    })
+      .then(num => {
+        if (num == 1) {
+          res.send({
+            message: "Artist was deleted successfully!"
+          });
+        } else {
+          res.send({
+            message: `Cannot delete Artist with id=${id}. Maybe Artist was not found!`
+          });
+        }
+      })
+      .catch(err => {
+        res.status(500).send({
+          message: "Could not delete Artist with id=" + id
+        });
+      });
+  };
+
+  // Delete all Artists from the database.
+  exports.deleteAll = (req, res) => {
+    Artist.destroy({
+      where: {},
+      truncate: false
+    })
+      .then(nums => {
+        res.send({ message: `${nums} Artists were deleted successfully!` });
+      })
+      .catch(err => {
+        res.status(500).send({
+          message:
+            err.message || "Some error occurred while removing all artists."
+        });
+      });
+  };
